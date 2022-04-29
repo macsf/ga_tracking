324759b707be2f9b57c347fa2e07174a1ad114e5
Sutthichok Wongthavorn <sutthichok@gmail.com>
2022-04-30 00:02:56 +0700

    Added several things.

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
 document.addEventListener('DOMContentLoaded', () => {
-
   // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll('.navbar-burger'),
+    0
+  );
+  const $serviceThumbs = Array.prototype.slice.call(
+    document.querySelectorAll('.thumb-service'),
+    0
+  );
+  const $highlightProducts = Array.prototype.slice.call(
+    document.querySelectorAll('#highlightProducts a'),
+    0
+  );
+  const $otherProducts = Array.prototype.slice.call(
+    document.querySelectorAll('#products a'),
+    0
+  );
 
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
-
     // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
+    $navbarBurgers.forEach((el) => {
       el.addEventListener('click', () => {
-
         // Get the target from the "data-target" attribute
         const target = el.dataset.target;
         const $target = document.getElementById(target);
 
-        console.log(target);
-
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle('is-active');
         $target.classList.toggle('is-active');
-
-
       });
     });
   }
 
+  if ($highlightProducts.length > 0) {
+    console.log($highlightProducts);
+
+    $otherProducts.forEach((el) => {
+      el.addEventListener('click', () => {
+        console.log(el);
+      });
+    });
+  }
+
+  if ($otherProducts.length > 0) {
+    $otherProducts.forEach((el) => {
+      el.addEventListener('click', () => {
+        alert(`Clicked on ${el.id}`);
+      });
+    });
+  }
+
+  if ($serviceThumbs.length > 0) {
+    $serviceThumbs.forEach((el) => {
+      el.addEventListener('click', () => {
+        alert(`Clicked on ${el.id}`);
+      });
+    });
+  }
 });
